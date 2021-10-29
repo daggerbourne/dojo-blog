f5ab30b6139631a0246d62c7ead712b90b48cf4e
DaggerBourne <valentino@daghertech.com>
2021-10-28 23:18:55 -0500

    first entries, beginning lesson 7

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>Dagher's on Point</h1>
+      <div className="links">
+        <a href="/">Home Page </a>
+        <a href="/create">New Blog</a>
+      </div> 
+    </nav>
+    );
+}
+ 
+export default Navbar;
